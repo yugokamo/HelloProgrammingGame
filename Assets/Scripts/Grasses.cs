35d6ff7faf66b247cd72ad71441f59b413982a50
Yugo Kamo <yugokamo@Yugo-no-MacBook-Pro.local>
2018-09-30 13:56:19 +0900

    Unityプロジェクトの雛形と、基本的な機能(ナビゲージョン、走る、ジャンプ)を追加

---
+++ Assets/Scripts/Grasses.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grasses : MonoBehaviour {
+    [SerializeField] private GameObject grassPrefab;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 100; i++) {
+            GameObject grass = Instantiate(grassPrefab) as GameObject;
+            grass.transform.SetParent(transform, false);
+            grass.transform.position += 2.56f * i * Vector3.right;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
